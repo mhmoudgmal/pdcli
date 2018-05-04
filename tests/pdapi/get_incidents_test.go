76a93c6543b9e3becb9984ef64f44641a22a6dc3
Mahmoud Gamal <mhmoudgmal.89@gmail.com>
2018-05-04 22:02:23 +0200

    Improve pdapi get-incidents specs

--- tests/pdapi/get_incidents_test.go
+++ tests/pdapi/get_incidents_test.go
@@ -18,55 +18,70 @@ var _ = Describe("pdapi", func() {
 		var ctx config.AppContext
 
 		BeforeEach(func() {
+
 			failuresChannel = make(chan string)
-			ctx = config.AppContext{FailuresChannel: &failuresChannel, PDConfig: &config.PDConfig{Email: "foo@bar.baz", Token: "pd_token"}}
+			ctx = config.AppContext{
+				FailuresChannel: &failuresChannel,
+				PDConfig: &config.PDConfig{
+					Email: "foo@bar.baz",
+					Token: "pd_token",
+				},
+			}
 		})
 
-		Context("when 200", func() {
+		Context("when request succeeds", func() {
 			incidentsString := `{
-				"incidents": 
+				"incidents":
 					[
 						{
-							"id": "PT4KHLK", 
-							"type": "incident", 
-							"html_url": "https://subdomain.pagerduty.com/incidents/PT4KHLK", 
-							"created_at": "2015-10-06T21:30:42Z", 
-							"status": "triggered", 
-							"service": { 
-								"id": "PIJ90N7", 
+							"id": "PT4KHLK",
+							"type": "incident",
+							"html_url": "https://subdomain.pagerduty.com/incidents/PT4KHLK",
+							"created_at": "2015-10-06T21:30:42Z",
+							"status": "triggered",
+							"service": {
+								"id": "PIJ90N7",
 								"summary": "My Mail Service"
 							}
 						}
 					]
 				}`
 
-			result := struct{ Incidents []models.Incident }{[]models.Incident{}}
-
-			It("returns the incidents and does not send any messages", func() {
+			JustBeforeEach(func() {
 				gock.New("https://api.pagerduty.com/incidents").
 					HeaderPresent("Authorization").
 					HeaderPresent("Accept").
 					Get("/").
 					Reply(200).
 					BodyString(incidentsString)
+			})
+
+			It("returns the incidents", func() {
+				result := struct{ Incidents []models.Incident }{[]models.Incident{}}
 
 				incidents := pdapi.GetIncidents(&ctx, map[string]string{})
-				Expect(*ctx.FailuresChannel).NotTo(Receive())
-
 				json.Unmarshal([]byte(incidentsString), &result)
-				Expect(incidents).To(Equal(result.Incidents))
 
+				Expect(incidents).To(Equal(result.Incidents))
+				Expect(gock.IsDone()).To(Equal(true))
+			})
+
+			It("does not send any messages to the failure chan", func() {
+				pdapi.GetIncidents(&ctx, map[string]string{})
+
+				Expect(*ctx.FailuresChannel).NotTo(Receive())
 				Expect(gock.IsDone()).To(Equal(true))
 			})
 		})
 
-		Context("when 400", func() {
+		Context("when bad request", func() {
 			It("sends error message through the failures channel", func() {
 				gock.New("https://api.pagerduty.com/incidents").
 					Get("/").
 					Reply(400)
 
 				go pdapi.GetIncidents(&ctx, map[string]string{})
+
 				Eventually(*ctx.FailuresChannel).Should(Receive(Equal("unexpected end of JSON input")))
 				Expect(gock.IsDone()).To(Equal(true))
 			})
