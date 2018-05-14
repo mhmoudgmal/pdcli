c4c18fe48aed3f6cdd970929e44b6289a414df4a
Mahmoud Gamal <mhmoudgmal.89@gmail.com>
2018-05-15 00:32:52 +0200

    Attempt refactoring CUI helpers

--- cui/helpers.go
+++ cui/helpers.go
@@ -3,46 +3,60 @@ package cui
 import (
 	ui "github.com/pdevine/termui"
 
-	. "pdcli/config"
+	"pdcli/config"
+	"pdcli/models"
 )
 
-func updateIncidentsWidget(ctx *AppContext, incidentList *ui.ListBox) {
+func updateIncidentsWidget(ctx *config.AppContext, lb *ui.ListBox) {
 	incidentsItems := []ui.Item{}
 
 	for {
 		select {
-		case incs := <-*ctx.IncidentsChannel:
-			func() {
-				for _, incident := range incs {
-					incidentsItems = append(
-						incidentsItems,
-						ui.Item{incident.ID, incident.Inspect()})
-				}
-
-				incidentList.Items = incidentsItems
-				incidentList.Height = len(incidentsItems) + 2
-
-				ui.Render(incidentList)
-			}()
+		case incidents := <-*ctx.IncidentsChannel:
+			incidentsItems = append(incidentsItems, mapIncidentsToUIItems(incidents)...)
+			updateIncidentListBox(lb, incidentsItems)
 
 		case incident := <-*ctx.UpdateStatusChannel:
-			func() {
-				itemIndex := func() int {
-					for idx, incItem := range incidentsItems {
-						if incItem.ItemVal == incident.ID {
-							return idx
-						}
-					}
-					return -1
-				}()
-
-				if itemIndex == -1 {
-					return
-				}
-
-				incidentList.Items[itemIndex] = ui.Item{incident.ID, incident.Inspect()}
-				ui.Render(incidentList)
-			}()
+			updateIncidentStatus(lb, incidentsItems, incident)
 		}
+	}
+}
+
+func mapIncidentsToUIItems(incidents []models.Incident) []ui.Item {
+	incidentsItems := []ui.Item{}
+
+	for _, incident := range incidents {
+		incidentsItems = append(
+			incidentsItems, ui.Item{
+				ItemVal: incident.ID,
+				Text:    incident.Inspect(),
+			},
+		)
+	}
+
+	return incidentsItems
+}
+
+func updateIncidentListBox(lb *ui.ListBox, incidentsItems []ui.Item) {
+	lb.Items = incidentsItems
+	lb.Height = len(incidentsItems) + 2
+	ui.Render(lb)
+}
+
+func updateIncidentStatus(lb *ui.ListBox, incidentsItems []ui.Item, incident models.Incident) {
+	itemIndex := -1
+
+	for idx, incidentItem := range incidentsItems {
+		if incidentItem.ItemVal == incident.ID {
+			itemIndex = idx
+		}
+	}
+
+	if itemIndex >= 0 {
+		lb.Items[itemIndex] = ui.Item{
+			ItemVal: incident.ID,
+			Text:    incident.Inspect(),
+		}
+		ui.Render(lb)
 	}
 }
