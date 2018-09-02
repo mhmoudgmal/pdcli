84a1f8f478a0d87cee9eb11476d519b739523b22
Mahmoud Gamal <mgamal@babbel.com>
2018-09-02 10:17:52 +0200

    Improve Cui notifibale

--- notifiable/cui/cui.go
+++ notifiable/cui/cui.go
@@ -1,11 +1,56 @@
 package cui
 
-import . "pdcli/i"
+import (
+	ui "github.com/pdevine/termui"
+
+	. "pdcli/i"
+	. "pdcli/notifiable/cui/helpers"
+)
 
 // Cui the command line interface notifiable or client.
 type Cui struct {
 	*AppContext
 	Widgets
+}
+
+// Init Initializes the app CUI
+func (Cui) Init(ctx *AppContext) {
+	defer ui.Close()
+
+	if err := ui.Init(); err != nil {
+		panic(err)
+	}
+
+	// create widgets
+	widgets := Widgets{
+		helpWidget(),
+		modeWidget(ctx),
+		incidentsWidget(),
+		onCallStatusWidget(),
+		incidentDetailsWidget(),
+	}
+
+	// build cui
+	ui.Body.AddRows(
+		ui.NewRow(
+			ui.NewCol(6, 0, widgets.HelpWidget),
+			ui.NewCol(6, 0, widgets.OnCallStatusWidget, widgets.ModeWidget),
+		),
+		ui.NewRow(
+			ui.NewCol(6, 0, widgets.IncidentsWidget),
+			ui.NewCol(6, 0, widgets.IncidentDetailsWidget),
+		),
+	)
+
+	// calculate layout and render
+	ui.Body.Align()
+	ui.Render(ui.Body)
+
+	go HandleEvents(ctx, widgets)
+	go UpdateIncidentsWidget(ctx, widgets.IncidentsWidget)
+	go UpdateIncidentDetailsWidget(ctx, widgets.IncidentDetailsWidget)
+
+	ui.Loop()
 }
 
 // Notify Cui
@@ -18,4 +63,9 @@ func (c Cui) Notify(msg string, data interface{}) {
 	case "detailed-incident":
 		*c.AppContext.IncidentDetailsChannel <- data.(IIncident)
 	}
+}
+
+// Clean cleans after you
+func (Cui) Clean() {
+	ui.Clear()
 }
